0c4c5b2ae83ca5a1bf317f18f0ebec78626fce9d
Sven Erik Adamson <50073041+SvenErikAdamson@users.noreply.github.com>
2023-02-07 16:48:46 +0200

    Cleanse

---
+++ scenes/entity/entity.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+class_name Entity
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
