20c4e7c4ef0f5dae3944d1aa2e372e5be12a9704
Sven Erik Adamson <50073041+SvenErikAdamson@users.noreply.github.com>
2024-05-21 13:56:38 +0300

    Clean

--- scenes/entity/item/item.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-class_name Item
-
-@export var amount: int = 1
-@export var item_instance: PackedScene = load("res://scenes/entity/item/item.tscn")
-var in_player_distance: bool = false
-@onready var sprite: Sprite2D = $ItemSprite
--- scenes/entity/player/player.gd
+++ scenes/entity/player/player.gd
@@ -7,10 +7,6 @@ var speed: float = 150
 var acceleration: float = 75
 var friction: float = acceleration / speed
 
-var items_around_player: Array[Item] = []
-var item_focused: Item = null
-var item_carried: PackedScene = null
-
 var last_direction: Vector2
 func ready():
 	set_velocity(Vector2.ZERO)
@@ -34,32 +30,4 @@ func get_input_direction() -> Vector2:
 func apply_friction(delta: float):
 	velocity -= velocity * friction * delta
 
-func _on_item_detection_area_body_entered(body: Node2D) -> void:
-	if body is Item:
-		if item_focused != null:
-			item_focused.modulate = ("ffffff")
-		item_focused = body as Item
-		item_focused.modulate = Color("60dd3c")
-		items_around_player.append(body)
 
-		
-func _on_item_detection_area_body_exited(body: Node2D) -> void:
-	if body is Item:
-		body.modulate = ("ffffff")
-		items_around_player.erase(body)
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") and item_focused != null:
-		item_carried = item_focused.item_instance
-		item_carried_sprite.show()
-		item_carried_sprite.texture = item_focused.sprite.texture
-		item_focused.queue_free()
-		item_focused = null
-		
-	elif event.is_action_pressed("interact") and item_focused == null and item_carried != null:
-		var item_instance = item_carried.instantiate() as Item
-		$"../Items".add_child(item_instance)
-		item_instance.global_position = global_position + get_input_direction()
-		item_carried_sprite.hide()
-		item_carried = null
-		
